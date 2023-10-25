93f5e8c2336bee1dee09fb6fbf5a9886475e3a22
Rulz029 <147622235+Rulz029@users.noreply.github.com>
2023-10-26 00:53:17 +0700

    __main__.py

--- AyiinXd/__main__.py
+++ AyiinXd/__main__.py
@@ -19,9 +19,9 @@ from traceback import format_exc
 from telethon import version
 from telethon.tl.alltlobjects import LAYER
 
-from AyiinXd import Ayiin, LOGS, LOOP, bot
-from AyiinXd.ayiin import HOSTED_ON, autobot, autopilot, checking, heroku
-from AyiinXd.modules import ALL_MODULES
+from Rulz import Rulz, LOGS, LOOP, bot
+from Rulz.Rul import HOSTED_ON, autobot, autopilot, checking, heroku
+from Rulz.modules import ALL_MODULES
 
 
 ON = '''
@@ -36,25 +36,25 @@ ON = '''
 '''
 
 
-async def AyiinMain():
+async def RulzMain():
     from config import var
 
-    await Ayiin.start()
+    await Rulz.start()
     if not var.BOTLOG_CHATID:
         await autopilot()
     if not var.BOT_TOKEN:
         await autobot()
     try:
         for module_name in ALL_MODULES:
-            imported_module = import_module(f"AyiinXd.modules.{module_name}")
+            imported_module = import_module(f"Rulz.modules.{module_name}")
         LOGS.info(f"Python Version - {python_version()}")
         LOGS.info(f"Telethon Version - {version.__version__} [Layer: {LAYER}]")
         LOGS.info(f"Userbot Version - {var.BOT_VER}")
         LOGS.info("[✨ BERHASIL DIAKTIFKAN! ✨]")
-        await checking(Ayiin)
-        me = await Ayiin.get_me()
+        await checking(Rulz)
+        me = await Rulz.get_me()
         bo = await bot.get_me()
-        await Ayiin.send_message(var.BOTLOG_CHATID, ON.format(var.BOT_VER, HOSTED_ON, me.id, me.first_name, bo.id, bo.first_name))
+        await Rulz.send_message(var.BOTLOG_CHATID, ON.format(var.BOT_VER, HOSTED_ON, me.id, me.first_name, bo.id, bo.first_name))
     except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
         pass
     except BaseException as e:
@@ -65,7 +65,7 @@ async def AyiinMain():
 if __name__ == "__main__":
     try:
         heroku()
-        LOOP.run_until_complete(AyiinMain())
+        LOOP.run_until_complete(Rulz())
     except BaseException:
         LOGS.error(format_exc())
         sys.exit()
@@ -74,6 +74,6 @@ if len(sys.argv) not in (1, 3, 4):
     Ayiin.disconnect()
 else:
     try:
-        Ayiin.run_until_disconnected()
+        Rulz.run_until_disconnected()
     except ConnectionError:
         pass
